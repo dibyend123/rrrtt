c5a5eae643faab02fbc4a3b6ea3ee6458a85a5f8
Ubuntu <ubuntu@ip-172-31-41-96.us-east-2.compute.internal>
2020-03-22 16:35:43 +0000

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+const routes: Routes = [
+  {
+     path:'',
+     redirectTo:'home',
+     pathMatch:'full'
+ },
 
-const routes: Routes = [];
+ {
+    path:'home',
+    component:HomePageComponent
+ }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
